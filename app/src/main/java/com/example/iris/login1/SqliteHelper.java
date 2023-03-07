95084f052897d71cf4e66e870cf89f10d80a23c3
Rishideep Chatterjee <82643613+rishideepc@users.noreply.github.com>
2023-03-08 04:18:52 +0530

    2023 02 16 rishideep dump & load user profile (#57)
    * 2023 02 16 rishideep dump & load user profile
    
    * 2023 02 23 rishideep dump & load user profiles: updated
    
    * 2023 03 02 rishideep dump & load user profiles: updated
    
    * 2023 03 04 Rishideep resolved JDK Merge conflict
    
    * 2023 03 06 Rishideep Dump and Load User profiles: updated

--- app/src/main/java/com/example/iris/login1/SqliteHelper.java
+++ app/src/main/java/com/example/iris/login1/SqliteHelper.java
@@ -1,11 +1,35 @@
 package com.example.iris.login1;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
+import android.util.Pair;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
 
 /**
  * Created by Iris on 16/7/20.
@@ -19,6 +43,9 @@ public class SqliteHelper extends SQLiteOpenHelper {
     public static final String TB_NAME = "users";
     public static final String REC_NAME = "last_login";
 
+    public String deviceId = Build.SERIAL;
+    private Date currentTime = new Date();
+    String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss.SSS").format(currentTime);
 
     public SqliteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -27,7 +54,7 @@ public class SqliteHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(
-                "CREATE TABLE IF NOT EXISTS "+
+                "CREATE TABLE IF NOT EXISTS " +
                         TB_NAME + "(" +
                         UserInfo.ID + " integer," +
                         UserInfo.USERICON + " varchar," +
@@ -40,21 +67,107 @@ public class SqliteHelper extends SQLiteOpenHelper {
                         ")"
         );
         db.execSQL(
-                "CREATE TABLE IF NOT EXISTS "+
+                "CREATE TABLE IF NOT EXISTS " +
                         REC_NAME + "(" +
                         UserInfo.ID + " integer," +
                         UserInfo.LAST_LOGIN_TIME + " varchar" +
                         ")"
         );
+        restoreUserProfiles();
         Log.e("database", "onCreate");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Cursor cursor = db.rawQuery("SELECT * FROM "+ TB_NAME, null);
+        if(cursor.moveToFirst()) {
+            dumpUserProfiles();
+        }
         db.execSQL("DROP TABLE IF EXISTS " + TB_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + REC_NAME);
+
         onCreate(db);
         Log.e("database", "onUpgrade");
+    }
+
+    @SuppressLint("Range")
+    private void dumpUserProfiles() {
+        try {
+            File dumpFileDir = new File(Common.RT_PATH + "/facelogin_userProfiles");
+            if(!dumpFileDir.exists()){
+                dumpFileDir.mkdirs();
+            }
+            File userProfileFile = new File(Common.RT_PATH + "/facelogin_userProfiles" + "/userProfile_" + deviceId + "_" + BuildConfig.VERSION_NAME + "_" + timestamp + ".json");
+            userProfileFile.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(userProfileFile);
+
+            JSONArray jsonArray = new JSONArray();
+
+            SQLiteDatabase db= getReadableDatabase();
+            Cursor cursor =db.rawQuery("SELECT * FROM "+ TB_NAME, null);
+            while(cursor.moveToNext()){
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("ID", cursor.getString(cursor.getColumnIndex(UserInfo.ID)));
+                jsonObject.put("USERICON", cursor.getString(cursor.getColumnIndex(UserInfo.USERICON)));
+                jsonObject.put("PROFILEICON", cursor.getString(cursor.getColumnIndex(UserInfo.PROFILEICON)));
+                jsonObject.put("USERVIDEO", cursor.getString(cursor.getColumnIndex(UserInfo.USERVIDEO)));
+                jsonObject.put("RECORDTIME", cursor.getString(cursor.getColumnIndex(UserInfo.RECORDTIME)));
+                jsonObject.put("GENDER", cursor.getString(cursor.getColumnIndex(UserInfo.GENDER)));
+                jsonObject.put("BIRTH_DEVICE", cursor.getString(cursor.getColumnIndex(UserInfo.BIRTH_DEVICE)));
+                jsonObject.put("BIRTH_DATE", cursor.getString(cursor.getColumnIndex(UserInfo.BIRTH_DATE)));
+                jsonArray.put(jsonObject);
+            }
+            cursor.close();
+            outputStream.write(jsonArray.toString().getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void restoreUserProfiles() {
+        try {
+            File dir = new File(Common.RT_PATH + "/facelogin_userProfiles");
+            File[] files = dir.listFiles();
+            Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
+            File restoreFile = files[0];
+            FileInputStream inputStream = new FileInputStream(restoreFile);
+            byte[] data = new byte[(int) restoreFile.length()];
+            inputStream.read(data);
+            inputStream.close();
+            String jsonString = new String(data);
+            insertUserProfiles(jsonString);
+            restoreFile.delete();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void insertUserProfiles(String jsonString){
+        try{
+            JSONArray jsonArray = new JSONArray(jsonString);
+            SQLiteDatabase db = getWritableDatabase();
+            db.beginTransaction();
+            for(int i=0; i<jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                ContentValues contentValues= new ContentValues();
+                contentValues.put(UserInfo.ID, jsonObject.getString("ID"));
+                contentValues.put(UserInfo.USERICON, jsonObject.getString("USERICON"));
+                contentValues.put(UserInfo.PROFILEICON, jsonObject.getString("PROFILEICON"));
+                contentValues.put(UserInfo.USERVIDEO, jsonObject.getString("USERVIDEO"));
+                contentValues.put(UserInfo.RECORDTIME, jsonObject.getString("RECORDTIME"));
+                contentValues.put(UserInfo.GENDER, jsonObject.getString("GENDER"));
+                contentValues.put(UserInfo.BIRTH_DEVICE, jsonObject.getString("BIRTH_DEVICE"));
+                contentValues.put(UserInfo.BIRTH_DATE, jsonObject.getString("BIRTH_DATE"));
+                db.insert(TB_NAME, null, contentValues);
+            }
+            db.setTransactionSuccessful();
+            db.endTransaction();
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+        }
     }
 
     public void updateColumn(SQLiteDatabase db, String name, String oldColumn, String newColumn, String typeColumn) {
